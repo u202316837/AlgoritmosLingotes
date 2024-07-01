8c69741f894ee963c9fd89f1be6fb1a44aeed0da
u202316837 <147280646+u202316837@users.noreply.github.com>
2024-06-30 19:42:10 -0500

    Initial commit

---
+++ src/Produccion.java
@@ -0,0 +1,33 @@
+public class Produccion {
+    /*
+    public static
+    Arreglo con datos del años anteriores
+     */
+    public static void main(String[] args) {
+        /*
+        Arreglo con inventario actual
+
+        variable demandaDelMes = metodo de prediccion(mes)
+
+        condicion
+        inventario seria suficienciente
+            productos terminados
+        o
+        no es suficiente
+            comprar materia prima
+
+            productos terminados
+         */
+
+        /*
+        generar informe de datos de otros años o actual
+         */
+    }
+
+    /*
+    metodo para pronosticar la demanda usando los datos del año pasado
+     */
+    /*
+    metodo para calucar la cantidad optima de materiaprima
+     */
+}
